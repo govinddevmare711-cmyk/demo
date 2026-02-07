ccfd934e5d1cb7b168b4b41b3dc860eafbbc5432
govinddevmare711-cmyk <govinddevmare711@gmail.com>
2026-02-07 12:58:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 (ap-south-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
 
